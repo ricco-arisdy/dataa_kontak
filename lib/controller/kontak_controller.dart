9e97a8f390f5e0153aab1169b2dc8d648dac6aff
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-28 19:59:32 +0700

    Implementasi KontakController, including handling of responses and error messages

--- lib/controller/kontak_controller.dart
+++ lib/controller/kontak_controller.dart
@@ -1,3 +1,48 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dataa_kontak/class/class_kontak.dart';
+import 'package:dataa_kontak/service/kontak_service.dart';
+
 class KontakController {
-  
+  final kontakService = KontakService();
+
+  Future<Map<String, dynamic>> addPerson(Kontak person, File? file) async {
+    Map<String, String> data = {
+      'nama': person.nama,
+      'nama': person.email,
+      'nama': person.alamat,
+      'nama': person.noTelepon,
+    };
+
+    try {
+      var response = await kontakService.addPerson(data, file);
+
+      if (response.statusCode == 201) {
+        return {
+          'success': true,
+          'message': 'Data berhasil disimpan',
+        };
+      } else {
+        if (response.headers['content-type']!.contains('aplication/json')) {
+          var decodedJson = jsonDecode(response.body);
+          return {
+            'success': false,
+            'message': decodedJson['message'] ?? 'Terjadi Kesalahan',
+          };
+        }
+        var decodedJson = jsonDecode(response.body);
+        return {
+          'success': false,
+          'message':
+              decodedJson['message'] ?? 'Terjadi Kesalahan saat menyimpan data',
+        };
+      }
+    } catch (e) {
+      return {
+        'success': false,
+        'message': 'Terjadi kesalahan: $e',
+      };
+    }
+  }
 }
