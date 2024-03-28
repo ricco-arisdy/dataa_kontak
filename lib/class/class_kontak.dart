7ad8a8f544776885f46b0ce29f8b18a974605cc3
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-28 19:10:26 +0700

    Implementasi Kontak class

---
+++ lib/class/class_kontak.dart
@@ -0,0 +1,15 @@
+class Kontak {
+  final String nama;
+  final String email;
+  final String alamat;
+  final String noTelepon;
+  final String foto;
+
+  Kontak({
+    required this.nama,
+    required this.email,
+    required this.alamat,
+    required this.noTelepon,
+    required this.foto,
+  });
+}
