e03a2a99b2c6c5a6b7c6fc3eb85ba67dea687403
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-28 18:56:51 +0700

    add variable, oject and select image

--- lib/wiget/form_kontak.dart
+++ lib/wiget/form_kontak.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FormKontak extends StatefulWidget {
   const FormKontak({super.key});
@@ -8,6 +11,28 @@ class FormKontak extends StatefulWidget {
 }
 
 class _FormKontakState extends State<FormKontak> {
+  File? _image;
+  final _imagePicker = ImagePicker();
+
+  final _formKey = GlobalKey<FormState>();
+  final _namaController = TextEditingController();
+  final _alamatController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _noTeleponController = TextEditingController();
+
+  Future<void> getImage() async {
+    final XFile? pickedFile =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected');
+      }
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
