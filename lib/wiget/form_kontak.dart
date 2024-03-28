50e7046ee2014752bf43252fa2d6ec2c3729c952
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-28 20:08:27 +0700

    Implementasi functional to save contact data

--- lib/wiget/form_kontak.dart
+++ lib/wiget/form_kontak.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:dataa_kontak/class/class_kontak.dart';
+import 'package:dataa_kontak/controller/kontak_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -74,10 +76,33 @@ class _FormKontakState extends State<FormKontak> {
               ),
             ),
             _image == null
-                  ? Text('Tidak ada gambar yang dipilih.')
-                  : Image.file(_image!),
-              ElevatedButton(
-                  onPressed: getImage, child: const Text('Pilih Gambar')),
+                ? Text('Tidak ada gambar yang dipilih.')
+                : Image.file(_image!),
+            ElevatedButton(
+                onPressed: getImage, child: const Text('Pilih Gambar')),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    var result = await KontakController().addPerson(
+                      Kontak(
+                        nama: _namaController.text,
+                        email: _emailController.text,
+                        alamat: _alamatController.text,
+                        noTelepon: _noTeleponController.text,
+                        foto: _image!.path,
+                      ),
+                      _image,
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(result['message'])),
+                    );
+                  }
+                },
+                child: const Text("Simpan"),
+              ),
+            )
           ]),
         ));
   }
