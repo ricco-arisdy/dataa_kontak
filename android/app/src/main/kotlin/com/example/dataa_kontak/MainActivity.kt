07c1eae8f559a8c564def2ab7f1d355ed95b0a95
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-28 18:46:29 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/dataa_kontak/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dataa_kontak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/wiget/form_kontak.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormKontak extends StatefulWidget {
+  const FormKontak({super.key});
+
+  @override
+  State<FormKontak> createState() => _FormKontakState();
+}
+
+class _FormKontakState extends State<FormKontak> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
