38bef9a9f8c855a462115000974366b3543218c6
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-28 19:34:26 +0700

    Implemented methods API in KontakService class

--- lib/service/kontak_service.dart
+++ lib/service/kontak_service.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
 class KontakService {
-  
+  final String baseUrl = 'https://paml.tatiumy.com/api/';
+  final String endpoint = 'people';
+
+  Uri getUri(String path) {
+    return Uri.parse("$baseUrl$path");
+  }
 }
