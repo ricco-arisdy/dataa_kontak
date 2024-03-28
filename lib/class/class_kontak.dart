f7de729399c5d4abf4e696d3c46b06bbc2ec8a63
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-28 19:22:36 +0700

    Added copyWith method and toMap

--- lib/class/class_kontak.dart
+++ lib/class/class_kontak.dart
@@ -12,4 +12,27 @@ class Kontak {
     required this.noTelepon,
     required this.foto,
   });
+  Kontak copyWith(
+      {String? nama,
+      String? email,
+      String? alamat,
+      String? noTelepon,
+      String? foto}) {
+    return Kontak(
+        nama: nama ?? this.nama,
+        email: email ?? this.email,
+        alamat: alamat ?? this.alamat,
+        noTelepon: noTelepon ?? this.noTelepon,
+        foto: foto ?? this.foto);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'nama': nama,
+      'email': email,
+      'alamat': alamat,
+      'no_telepon': noTelepon,
+      'foto': foto
+    };
+  }
 }
