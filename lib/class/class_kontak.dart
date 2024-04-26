7ccac8b8970eadeb570f794ae49fb33d4998672d
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-04-27 03:33:13 +0700

    add deklarasi map controller

--- lib/class/class_kontak.dart
+++ lib/class/class_kontak.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Kontak {
   final String nama;
   final String email;
@@ -34,5 +36,45 @@ class Kontak {
       'no_telepon': noTelepon,
       'foto': foto
     };
+  }
+
+  factory Kontak.fromMap(Map<String, dynamic> map) {
+    return Kontak(
+      nama: map['nama'] as String,
+      email: map['email'] as String,
+      alamat: map['alamat'] as String,
+      noTelepon: map['noTelepon'] as String,
+      foto: map['foto'] as String,
+    );
+  }
+  
+  String toJson() => json.encode(toMap());
+
+  factory Kontak.fromJson(String source) => Kontak.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Kontak(nama: $nama, email: $email, alamat: $alamat, noTelepon: $noTelepon, foto: $foto)';
+  }
+
+  @override
+  bool operator ==(covariant Kontak other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.nama == nama &&
+      other.email == email &&
+      other.alamat == alamat &&
+      other.noTelepon == noTelepon &&
+      other.foto == foto;
+  }
+
+  @override
+  int get hashCode {
+    return nama.hashCode ^
+      email.hashCode ^
+      alamat.hashCode ^
+      noTelepon.hashCode ^
+      foto.hashCode;
   }
 }
--- lib/screen/map_screen.dart
+++ lib/screen/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   final Function(String) onLocationSelected;
@@ -10,6 +11,9 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   
+  late GoogleMapController mapController;
+  LatLng? _lastMapPosition;
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
